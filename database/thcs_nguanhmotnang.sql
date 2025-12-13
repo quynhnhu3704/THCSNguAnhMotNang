b1fff0b8ec31c6cf384cd66704c33b9c5a239fac
quynhnhu3704 <22663381.nhu@student.iuh.edu.vn>
2025-12-13 18:17:40 +0700

    chore: push full project structure

--- database/thcs_nguanhmotnang.sql
+++ database/thcs_nguanhmotnang.sql
@@ -17,6 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+USE thcs_nguanhmotnang;
+
 --
 -- Cơ sở dữ liệu: `thcs_nguanhmotnang`
 --
